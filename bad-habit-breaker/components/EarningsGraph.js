6ff41af9e6cdf1c1d78b90e911f9f01f08a5c067
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-09-23 19:39:40 +1000

    Remove previous earnings graph, changed total earnings to total savings

--- bad-habit-breaker/components/EarningsGraph.js
+++ bad-habit-breaker/components/EarningsGraph.js
@@ -1,29 +1,25 @@
-const EarningsGraph = ({ section, earningsSmoking, earningsDrinking, allClicked, totalSquares }) => {
+const EarningsGraph = ({ section, earningsSmoking, earningsDrinking }) => {
   const maxWidth = 300; // Adjust this based on your design
   let barWidth;
 
-  // Calculate the ratio of clicked squares to total squares
-  const ratio = allClicked ? 1 : totalSquares > 0 ? earningsSmoking / (totalSquares * 25) : 0;
-
-  barWidth = maxWidth * ratio;
+  if (section === "Smoking") {
+    barWidth = Math.min(maxWidth, earningsSmoking);
+  } else if (section === "Drinking") {
+    barWidth = Math.min(maxWidth, earningsDrinking);
+  }
 
   // Calculate the total earnings based on the section-specific earnings
-  const totalEarnings =
-    section === "Smoking" ? earningsSmoking : section === "Drinking" ? earningsDrinking : 0;
+  const totalEarnings = section === "Smoking" ? earningsSmoking : earningsDrinking;
 
-  // Check if totalEarnings is a number before formatting
-  const formattedEarnings =
-    typeof totalEarnings === "number" ? totalEarnings.toFixed(2) : "0.00";
+  // Format the total earnings with two decimal places
+  const formattedEarnings = totalEarnings.toFixed(2);
 
-  // Change the label and remove the "Earnings Graph" text
   return (
     <div className="mt-4">
-      <div className="bg-gray-300 h-8 rounded-md">
-        <div
-          className="bg-green-500 h-8 rounded-md"
-          style={{ width: `${barWidth}px` }}
-        ></div>
-      </div>
+     
+      
+        
+      
       <p className="mt-2">Total Savings: ${formattedEarnings}</p>
     </div>
   );
